fa720f412521f2c55d7d38a2bff276fc4a7cb0ac
Minseok Lee <maldarkmlee@gmail.com>
2020-07-17 08:11:39 +0900

    initial setting

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "Pages/Home";
-import Login from "Pages/Login";
+import Intro from "Pages/Intro";
+import Signup from "Pages/SignUp";
+import Upload from "Pages/Upload";
+import Processing from "Pages/Processing";
+import Download from "Pages/Download";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Intro} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/upload" component={Upload} />
+          <Route exact path="/processing" component={Processing} />
+          <Route exact path="/download" component={Download} />
         </Switch>
       </Router>
     );
