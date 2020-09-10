e266707c866456b616f73c87384da13b167512ab
Minseok Lee <maldarkmlee@gmail.com>
2020-09-11 00:51:03 +0900

    Updates

---
+++ precache-manifest.133a77a3f4b02b412b7c28d6612b9eec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6c2361804fa7bc895f6eb3c80810a9",
+    "url": "/JQ_merch/index.html"
+  },
+  {
+    "revision": "88722610b9c98a4b59ee",
+    "url": "/JQ_merch/static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "d71a3cb07e290de4d91d",
+    "url": "/JQ_merch/static/css/main.b8941c20.chunk.css"
+  },
+  {
+    "revision": "88722610b9c98a4b59ee",
+    "url": "/JQ_merch/static/js/2.99229cdd.chunk.js"
+  },
+  {
+    "revision": "f93367ef6b2e30658503da241009c55c",
+    "url": "/JQ_merch/static/js/2.99229cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71a3cb07e290de4d91d",
+    "url": "/JQ_merch/static/js/main.a094a036.chunk.js"
+  },
+  {
+    "revision": "e47751d4b262afb9655e",
+    "url": "/JQ_merch/static/js/runtime-main.2226b163.js"
+  },
+  {
+    "revision": "a288961d62681aeff38489afad616c28",
+    "url": "/JQ_merch/static/media/notokr-regular.a288961d.woff2"
+  }
+]);
